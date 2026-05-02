e475fa65adeb1f65e9b366f3909ca5562a3b293a
Harsh Jain <harshf9@proton.me>
2026-05-02 11:33:35 +0530

    Add solution for pytorch-tensor-reshape

---
+++ study-plans/pytorch-basics/pytorch-tensor-reshape/pytorch-tensor-reshape.py
@@ -0,0 +1,12 @@
+import torch
+
+def reshape_tensor(x, op):
+    """Returns a List"""
+    t= torch.tensor(x,dtype=torch.float32)
+    if op=="flatten":
+        return t.flatten().tolist()
+    elif op=="squeeze":
+        return t.squeeze().tolist()
+    elif op=="transpose":
+        return t.T.tolist()
+        
